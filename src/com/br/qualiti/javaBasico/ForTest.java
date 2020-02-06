e40af3ec3ffb849128aa03ba31027c66d0f75cde
RicardoMVA <ricardo-valenca@hotmail.com>
2020-02-06 15:07:15 -0300

    add solution to 'for' test

--- src/com/br/qualiti/javaBasico/ForTest.java
+++ src/com/br/qualiti/javaBasico/ForTest.java
@@ -2,4 +2,10 @@ package com.br.qualiti.javaBasico;
 
 public class ForTest {
 
+	public static void main(String[] args) {
+		for (int i = 0; i < 11; i++) {
+			System.out.println("Contagem: " + i);
+		}
+	}
+
 }
