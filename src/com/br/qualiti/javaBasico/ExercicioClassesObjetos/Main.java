6fbe2c951f1e7b4eace300cf9e8922b7ec60b921
RicardoMVA <ricardo-valenca@hotmail.com>
2020-02-10 16:11:41 -0300

    add operation in main to trigger exception in 'transferencia' from 'debitar'

--- src/com/br/qualiti/javaBasico/ExercicioClassesObjetos/Main.java
+++ src/com/br/qualiti/javaBasico/ExercicioClassesObjetos/Main.java
@@ -5,21 +5,30 @@ public class Main {
 	public static void main(String[] args) {
 		Conta c1 = new Conta("123", "Ricardo");
 		Conta c2 = new Conta("321", "Ivan");
-		
+
 		c1.setSaldo(100);
 		c2.setSaldo(0);
-		
+
 		System.out.println("Saldo da conta " + c1.getNumero() + ": " + c1.getSaldo());
 		System.out.println("Saldo da conta " + c2.getNumero() + ": " + c2.getSaldo());
-		
-		c1.debitar(50);
+
+		try {
+			c1.debitar(50);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 		c2.creditar(50);
-		
+
 		System.out.println("Saldo da conta " + c1.getNumero() + ": " + c1.getSaldo());
 		System.out.println("Saldo da conta " + c2.getNumero() + ": " + c2.getSaldo());
-		
-		c1.transferir(c2, 10);
-		
+
+		try {
+			c1.transferir(c2, 200);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 		System.out.println("Saldo da conta " + c1.getNumero() + ": " + c1.getSaldo());
 		System.out.println("Saldo da conta " + c2.getNumero() + ": " + c2.getSaldo());
 
