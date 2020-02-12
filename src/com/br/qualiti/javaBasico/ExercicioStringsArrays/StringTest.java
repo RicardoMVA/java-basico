728742d6dc04000194ab48df4d0f9d75c0e1569f
RicardoMVA <ricardovalenca@gmail.com>
2020-02-11 21:11:00 -0300

    answers for itens 7 to 9

--- src/com/br/qualiti/javaBasico/ExercicioStringsArrays/StringTest.java
+++ src/com/br/qualiti/javaBasico/ExercicioStringsArrays/StringTest.java
@@ -11,6 +11,19 @@ public class StringTest {
 		System.out.println(s1.equals(s2));
 		
 		System.out.println(s1 == s2);
+		
+		String s3 = "Ivan";
+		String s4 = "Ivan";
+		
+		System.out.println(s3.equals(s4));
+		
+		System.out.println(s3 == s4);
+		
+		s3 = "Matheus";
+		
+		System.out.println(s3.equals(s4));
+		
+		System.out.println(s3 == s4);
 
 	}
 
