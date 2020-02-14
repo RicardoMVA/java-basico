f0c07720bfab65cbf900307c386939105662c9fc
RicardoMVA <ricardovalenca@gmail.com>
2020-02-13 21:42:03 -0300

    put value that makes sense in 'taxa' attribute

--- src/com/br/qualiti/javaBasico/QualitiBank/Poupanca.java
+++ src/com/br/qualiti/javaBasico/QualitiBank/Poupanca.java
@@ -2,7 +2,7 @@ package com.br.qualiti.javaBasico.QualitiBank;
 
 public class Poupanca extends Conta {
 	
-	private static double taxa;
+	private static double taxa = 1.01;
 	
 	public Poupanca(String numero, double saldo, Cliente cliente) {
 		super(numero, saldo, cliente);
