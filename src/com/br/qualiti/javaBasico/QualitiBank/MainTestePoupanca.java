3c520b14a26cdbb809004eb5a0693df97190d5b4
RicardoMVA <ricardovalenca@gmail.com>
2020-02-13 21:44:58 -0300

    make main to test class 'poupanca'

---
+++ src/com/br/qualiti/javaBasico/QualitiBank/MainTestePoupanca.java
@@ -0,0 +1,17 @@
+package com.br.qualiti.javaBasico.QualitiBank;
+
+public class MainTestePoupanca {
+	public static void main(String[] args) {
+		Cliente c1 = new Cliente("Ricardo", "12312312310", TipoCliente.VIP);
+		
+		Poupanca p1 = new Poupanca("123", 12345, c1);
+		
+		System.out.println("Cliente: " + p1.getCliente().getNome());
+		
+		System.out.println("Saldo atual: " + p1.getSaldo());
+		
+		p1.renderJuros();
+		
+		System.out.println("Saldo atual depois de render: " + p1.getSaldo());
+	}
+}
